710f468ea6063a9517a167f8f745cb46e15f3b5d
mfaishal82 <mfaishal82@yahoo.co.id>
2024-04-16 19:34:35 +0700

    refactor: splash screen

--- MediChatRx/App.js
+++ MediChatRx/App.js
@@ -1,7 +1,24 @@
+import React, { useState, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
+  const [isReady, setIsReady] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsReady(true);
+    }, 2000); // 2 seconds splash screen delay
+  }, []);
+
+  if (!isReady) {
+    return (
+      <View style={styles.splashScreen}>
+        <Image source={require('./assets/i-icon.png')} style={styles.splashImage} />
+      </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
@@ -16,5 +33,15 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  splashScreen: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#fff',
+  },
+  splashImage: {
+    width: 100,
+    height: 100,
   },
 });
