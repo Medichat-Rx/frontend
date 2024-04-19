d2b7893be0dba70d062e4672e48a91181d331dc9
mfaishal82 <mfaishal82@yahoo.co.id>
2024-04-19 10:06:39 +0700

    refactor: re-add splash screen

--- MediChatRx/App.js
+++ MediChatRx/App.js
@@ -2,15 +2,16 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
 // import 'react-native-gesture-handler';
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StyleSheet, Text, View, Button, Image } from "react-native"; // Added Image import
 import { ApolloProvider } from "@apollo/client";
 import client from "./config/apolloClient";
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "./context/AuthContext";
 import * as SecureStore from "expo-secure-store";
-import MyDrawer from "./navigators/MyDrawer"; // Changed from MyTabs to MyDrawer
+import MyDrawer from "./navigators/MyDrawer";
 
 export default function App() {
+  const [isReady, setIsReady] = useState(false);
   const [isSignedIn, setIsSignedIn] = useState(true);
   // const { isSignedIn, setIsSignedIn } = useContext(AuthContext);
 
@@ -27,9 +28,20 @@ export default function App() {
   };
 
   useEffect(() => {
+    setTimeout(() => {
+      setIsReady(true);
+    }, 1000); // 2 seconds splash screen delay
     getToken();
     // console.log(getToken())
   }, []);
+
+  if (!isReady) {
+    return (
+      <View style={styles.splashScreen}>
+        <Image source={require('./assets/i-icon.png')} style={styles.splashImage} />
+      </View>
+    );
+  }
 
   return (
     <AuthContext.Provider
@@ -53,5 +65,15 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  splashScreen: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#fff",
+  },
+  splashImage: {
+    width: 100,
+    height: 100,
   },
 });
