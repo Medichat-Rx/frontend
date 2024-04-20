4a64fedf7cf690d0cde0a57f0ecdb8ffa067a014
mfaishal82 <mfaishal82@yahoo.co.id>
2024-04-21 04:02:17 +0700

    refactor: add expo location and integration with expo map view

--- MediChatRx/screens/MapScreen.js
+++ MediChatRx/screens/MapScreen.js
@@ -1,12 +1,22 @@
-import React, { useState, useEffect } from 'react';
-import { StyleSheet, View, TextInput, Button, ActivityIndicator } from 'react-native';
-import MapView, { Marker } from 'react-native-maps';
-import tw from 'tailwind-react-native-classnames'; // Added import for tailwind styles
+import React, { useState, useEffect } from "react";
+import {
+  Platform,
+  StyleSheet,
+  View,
+  TextInput,
+  Button,
+  ActivityIndicator,
+  Text,
+} from "react-native";
+import MapView, { Marker } from "react-native-maps";
+import tw from "tailwind-react-native-classnames";
+import * as Location from "expo-location";
 
 export default function MapScreen() {
-  const [searchQuery, setSearchQuery] = useState('');
-  const [searchResult, setSearchResult] = useState(null);
+  const [searchQuery, setSearchQuery] = useState("");
   const [loading, setLoading] = useState(false);
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
 
   const handleSearch = () => {
     // Implementasikan logika untuk mencari lokasi berdasarkan searchQuery
@@ -14,30 +24,64 @@ export default function MapScreen() {
   };
 
   useEffect(() => {
-    if (searchQuery !== '') {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+
+    if (searchQuery !== "") {
       // setLoading(true);
       handleSearch();
     }
   }, [searchQuery]);
 
+  let text = "Waiting..";
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
+  }
+
   return (
     <View style={tw`flex-1`}>
       <TextInput
-        style={tw`h-12 border border-gray-300 rounded-full m-2 pl-7`} // Updated style to use tailwind
+        style={tw`h-12 border border-gray-300 rounded-full m-2 pl-7`}
         placeholder="Tentukan lokasimu saat ini..."
         value={searchQuery}
-        onChangeText={text => setSearchQuery(text)}
+        onChangeText={(text) => setSearchQuery(text)}
       />
       <Button title="Search" onPress={handleSearch} color="#0050ff" />
       {loading ? (
         <ActivityIndicator size="large" color="#0000ff" />
       ) : (
-        <MapView style={tw`flex-1`}>
-        {searchResult && <Marker coordinate={searchResult} />}
-        </MapView>
+        location && location.coords && (
+          <MapView
+            style={tw`flex-1`}
+            initialRegion={{
+              latitude: location.coords.latitude,
+              longitude: location.coords.longitude,
+              latitudeDelta: 0.0922,
+              longitudeDelta: 0.0421,
+            }}
+          >
+            <Marker
+              coordinate={{
+                latitude: location.coords.latitude,
+                longitude: location.coords.longitude,
+              }}
+              title="Lokasi Anda"
+              description="Ini adalah lokasi Anda."
+            />
+          </MapView>
+        )
       )}
+
     </View>
   );
 }
-
-const styles = StyleSheet.create({}); // Removed styles constant as tailwind is used for styling
