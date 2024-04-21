c9a1c0088dbd174d87a05f942691f1bd874247b0
raihank43 <raihankusuma5@gmail.com>
2024-04-21 23:11:52 +0700

    feat: send message

--- MediChatRx/screens/HomeScreen.js
+++ MediChatRx/screens/HomeScreen.js
@@ -1,8 +1,9 @@
-import { useQuery } from "@apollo/client";
+import { useMutation, useQuery } from "@apollo/client";
 import { Chat, defaultTheme } from "@flyerhq/react-native-chat-ui";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { GET_CHAT_MESSAGE } from "../queries/GetChatMessage";
 import Loading from "../components/LoadingComponent";
+import { SEND_MESSAGE } from "../mutations/SendMessageMutation";
 
 // For the testing purposes, you should probably use https://github.com/uuidjs/uuid
 const uuidv4 = () => {
@@ -16,8 +17,28 @@ const uuidv4 = () => {
 export default function HomeScreen() {
   const [messages, setMessages] = useState([]);
   const { loading, error, data } = useQuery(GET_CHAT_MESSAGE);
+  const [sendMessage] = useMutation(SEND_MESSAGE);
 
   // const user = { id: "06c33e8b-e835-4736-80f4-63f44b66666c" };
+
+  const UserId = data?.getChatMessage.UserId;
+  const user = { id: UserId };
+
+  useEffect(() => {
+    if (data) {
+      const formattedMessages = data.getChatMessage.message
+        .slice()
+        .reverse()
+        .map((el) => ({
+          id: el._id,
+          author: el.UserId === UserId ? { id: UserId } : { id: "ChatBot" },
+          text: el.text,
+          type: "text",
+          createdAt: Number(el.createdAt),
+        }));
+      setMessages(formattedMessages);
+    }
+  }, [data]); // Dependensi useEffect adalah data
 
   const addMessage = (message) => {
     setMessages([message, ...messages]);
@@ -31,33 +52,21 @@ export default function HomeScreen() {
       text: message.text,
       type: "text",
     };
-    addMessage(textMessage);
+    // addMessage(textMessage);
+    setMessages([textMessage, ...messages]);
+    sendMessage({
+      variables: {
+        newMessage: {
+          text: message.text,
+        },
+      },
+    });
   };
 
   if (loading) {
     console.log("masuk");
     return <Loading />;
   }
-
-  console.log(Date.now())
-
-  const UserId = data.getChatMessage.UserId;
-  const user = { id: UserId };
-  const chatMessages = data.getChatMessage.message
-    .slice() // Membuat salinan array untuk mencegah mutasi pada array asli
-    .reverse() // Membalik urutan array
-    .map((el) => {
-      let newEl = {
-        id: el._id,
-        author: el.UserId == UserId ? { id: UserId } : { id: "ChatBot" },
-        text: el.text,
-        type: "text",
-        createdAt: Number(el.createdAt),
-      };
-      return newEl;
-    });
-
-  console.log(chatMessages, "data chat message");
 
   return (
     // Remove this provider if already registered elsewhere
@@ -70,7 +79,7 @@ export default function HomeScreen() {
       l10nOverride={{ inputPlaceholder: "Ada yang bisa kami bantu?" }}
       locale="en"
       // renderCustomMessage={data.getChatMessage.message}
-      messages={chatMessages}
+      messages={messages}
       enableAnimation="true"
       onSendPress={handleSendPress}
       user={user}
